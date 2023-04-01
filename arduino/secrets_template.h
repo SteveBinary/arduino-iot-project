5528add2aa30f44a4d399d960dc41a0da6f87063
SteveBinary <SteveBinary@users.noreply.github.com>
2023-04-01 18:36:39 +0200

    Add template for secrets file.

---
+++ arduino/secrets_template.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define WIFI_HOSTNAME "my-iot-sensor"
+
+#define MQTT_CLIENT_ID "my-client"
+#define MQTT_BROKER "my.broker.com"
+#define MQTT_PORT 1883
+#define MQTT_USE_CREDENTIALS true
+#define MQTT_USER ""
+#define MQTT_PASSWORD ""
+
+#define MQTT_TOPIC_TEMPERATURE "test/temperature"
+#define MQTT_TOPIC_PRESSURE "test/pressure"
+#define MQTT_TOPIC_HUMIDITY "test/humidity"
+#define MQTT_TOPIC_AIR_QUALITY "test/airquality"
+#define MQTT_TOPIC_LIGHT_INTENSITY "test/lightintesity"
+#define MQTT_TOPIC_ALL_MEASUREMENTS "test/all"
